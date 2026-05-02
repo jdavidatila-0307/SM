5953dfd8e61d46dddbb6a09e6abca53a6fdaa61c
jdavidatila-0307 <jdavidatila@gmail.com>
2026-05-02 17:23:14 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,11 +2,13 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: null,
-    checkServerIdentity: () => undefined
-  }
+  ssl: { rejectUnauthorized: false },
+  connectionTimeoutMillis: 5000,
+});
+
+// Añadir evento de error para depuración
+pool.on('error', (err) => {
+  console.error('[db] Error inesperado en pool:', err.message);
 });
 
 module.exports = pool;
